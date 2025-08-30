2b286aeff1008db6673eed551671da4837be7b30
KUNIMITSU ASANO <mittsulann73@gmail.com>
2025-08-30 21:14:19 +0900

    白い画面修正: HTML更新、デバッグログ追加

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import "./styles.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+console.log('Starting React app...');
+
+const container = document.getElementById("root");
+if (!container) {
+  console.error('Root element not found!');
+} else {
+  console.log('Root element found, creating React root...');
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+  console.log('React app rendered');
+}
